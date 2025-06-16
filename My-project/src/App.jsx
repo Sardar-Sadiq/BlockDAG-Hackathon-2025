1c1d30b8fd7241b6227868f1a9197731615853d0
Sardar-Sadiq <sardarsadiq106@gmail.com>
2025-06-16 13:24:46 +0530

    created the vite app with tailwindcss

---
+++ My-project/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className='bg-blue-950 h-screen flex justify-center items-center'>
+      <h1 className='text-2xl font-extralight text-white '>hello</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
